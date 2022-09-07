53bb942ab6252984ae9087d1e749f82113f85c6d
Matheus de Souza <matheusdesouza078@gmail.com>
2022-09-07 11:53:33 -0300

    Changed jwt secret

--- AuthJwt/Settings.cs
+++ AuthJwt/Settings.cs
@@ -2,6 +2,6 @@
 {
     public static class Settings
     {
-        public static string Secret = "fedaf7d8863b48e197b9287d492b708e";
+        public static string Secret = "cj8mUHeuB3j0TjO69qPyUKRlnx11WRDQ6MtmUTq05vhDklhdMwz0Phdr7qOjvXF";
     }
 }
